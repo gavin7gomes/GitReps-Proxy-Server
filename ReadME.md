e09a0590b3bc025ebe36526c4a0607580fb558ee
gavin7gomes <gavinrockgomes@gmail.com>
2023-09-15 15:17:45 +0530

    Initial commit of proxy server

---
+++ ReadME.md
@@ -0,0 +1,19 @@
+# My Node.js Application
+
+Welcome to my Node.js application! This application acts as a proxy to a react application that directs API requests to github developer APIs to eliminate CORS issues.
+
+## Getting Started
+
+To get started with this application, follow these steps:
+
+1. Clone the repository: `git clone https://github.com/yourusername/your-repo.git`
+2. Install dependencies: `npm install`
+3. Run the application: `npm start`
+
+## Usage
+
+You can use this application by doing the following:
+
+```sh
+npm start
+```
---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var cors = require('cors');
+
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+var bodyParser = require('body-parser');
+
+const CLIENT_SECRET = "7f9f5ab59e2ba1590af0e207226bb3f5585546a8";
+const CLIENT_ID = "b1b55489f40c124966a1";
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/getAccessToken", async function (req, res) {
+    const params = "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=" +  req.query.code;
+
+    await fetch("https://github.com/login/oauth/access_token" + params, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json"
+        }
+    }).then((response) => {
+        return response.json();
+    }).then(async (data) => {
+        console.log(data);
+        await fetch("https://api.github.com/user", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${data.access_token}`
+            }
+        }).then((response) => {
+            return response.json();
+        }).then((data2) => {
+            res.json({...data, ...data2});
+        })
+    })
+})
+
+
+app.get("/getUserData", async function (req, res) {
+        await fetch("https://api.github.com/user", {
+            method: "GET",
+            headers: {
+                "Authorization": req.get("Authorization")
+            }
+        }).then((response) => {
+            return response.json();
+        }).then((data) => {
+            res.json(data);
+        })
+})
+
+app.listen(4000, function(){
+    console.log("CORS server running on port 4000");
+})
